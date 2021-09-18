2ea54e53c7bb2f4af0a5735f6f3471bc2435fa05
MoinPatel <moinpatel1992@gmail.com>
2021-09-17 22:47:52 -0400

    added minor update

--- src/Header.js
+++ src/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
                             </p>
                             <h3>Get early access for you</h3>
                             <div className="input-group mt-3">
-                                <input type="text" className="rounded-pill w-75 w-lg-75 me-3 p-2 form-control-text" placeholder="Enter Your Email" />
+                                <input type="text" className="rrounded-pill w-50  w-lg-75 me-3 p-2 form-control-text" placeholder="Enter Your Email" />
                                 <div className="input-group-button">Get it Now</div>
                             </div>
                         </div>
